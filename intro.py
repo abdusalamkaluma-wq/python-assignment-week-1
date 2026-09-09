6c150aa47ab19a0b8ed635d2b8f971a12eff8cf1
abdusalamkaluma-wq <abdusalamkaluma@gmail.com>
2026-09-09 22:08:25 +0300

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,9 @@
+# this program introduce me using variable and prinit statements
+name = "Kaluma"
+age = "27"
+favourite language "python"
+goal = "to build practical soft wire skils I can use in real life project"
+prinit(f"hell! my name is{name}.")
+prinit(f"I am {age} years old.")
+prinit(f"My favourite programming language is {favourite_language}.")
+prinit (f"My goal for this course is {goal}.")
